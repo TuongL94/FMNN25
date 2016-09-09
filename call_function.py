cdbe948eb2a7a9348121cfa9affe3a0e41ab5ba8
AnMiKa <AnMiKa@users.noreply.github.com>
2016-09-09 13:48:24 +0200

    __call__ more or less finished
    added Error messages and finished __call function (only error messages are not as pretty as they could be).

--- call_function.py
+++ call_function.py
@@ -10,9 +10,14 @@ from  pylab import *
 
 #class for representing a cubic spline
 class CSpline:
-    def __init__(self,controlPoints,nodes):
+    def __init__(self,controlPoints,nodes,polDeg=3):
+        if len(controlPoints) < polDeg:
+            raise Exception('Number of control points needs to at least', polDeg)
+        if len(nodes) < len(controlPoints)+2:
+            raise Exception('Number of nodes needs to at least', polDeg+2)
         self._controlPoints=controlPoints
         self._nodes=nodes
+        self._polDeg=polDeg
     def getControlPoints(self):
         return self._controlPoints
     def setControlPoints(self,controlPoints):
@@ -24,28 +29,29 @@ class CSpline:
         
     controlPoints=property(getControlPoints,setControlPoints)
     
-    def __call__(self,controlPoints,nodes):
+    def __call__(self,u):
         # find hot interval
-        IPlusOne=(nodes > self). argmax ()
-        # error message when u not at right place?
-  ###### change for 2- or n-dimensional points d!!!      
+        iPlusOne=(self._nodes > u). argmax ()
+        # error message when u not in possible interval
+        if iPlusOne < 3 or iPlusOne > len(self._nodes)-2:
+            raise Exception('Choose u between', self._nodes[2], self._nodes[len(self._nodes)-2])
         # collect needed controlpoints
-        d=array(controlPoints[IPlusOne-3:IPlusOne+1])
+        d=array(self._controlPoints[iPlusOne-3:iPlusOne+1])
         # calculate CSpline s(u) by DeBoor-algorithm
-        for i in range(2,-1,-1):
+        for i in range(self._polDeg-1,-1,-1):
             for k in range(0,i):
-                alpha=(nodes[IPlusOne+k]-self)/(nodes[IPlusOne+k]-nodes[IPlusOne-3+k+i])
+                alpha=(self._nodes[iPlusOne+k]-u)/(self._nodes[iPlusOne+k]-self._nodes[iPlusOne-3+k+i])
                 d[k]=alpha*d[k]+(1-alpha)*d[k+1]  
-        s=d[0]
-        return s
+        return d[0]
         
         
         
     #def plot(self):
         
     #def getBasisFunction(self,nodes,j):
+        
+s=CSpline(array([[1,2],[3,4]]),array([0,1,2,5,7]))
+u=1.9
+print(s(u))
 
-s=CSpline(1,2)
-s.__call__(u)
-s(u)
-print(s._controlPoints)
+#print(s._controlPoints)
