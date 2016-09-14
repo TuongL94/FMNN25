2c46f1e95387c07fdca4123f5d4a6066330c0fcb
BernhardPoe <bernhardp@gmx.net>
2016-09-14 12:49:45 +0200

    Add files via upload

--- CSpline.py
+++ CSpline.py
@@ -40,33 +40,40 @@ class CSpline:
     def __call__(self,u):
         # find hot interval
         iPlusOne=self.getHotInterval(u)
+        #special case (if u==last possible node)
+        if iPlusOne == len(self._nodes)-2 and u==self._nodes[iPlusOne-1]:
+            iPlusOne=iPlusOne-1
         # error message when u not in possible interval
-        if iPlusOne < 3 or iPlusOne > len(self._nodes)-2:
-            raise Exception('Choose u between', self._nodes[2], self._nodes[len(self._nodes)-2])
+        if iPlusOne < 3 or iPlusOne >= len(self._nodes)-2:
+            raise Exception('Choose u between', self._nodes[2], self._nodes[len(self._nodes)-3])
         # collect needed controlpoints
         d=array(self._controlPoints[iPlusOne-3:iPlusOne+1])
         # calculate CSpline s(u) by DeBoor-algorithm
         for i in range(0,self._polDeg):
             for k in range(0,self._polDeg-i):
                 #calc the denom
-                alpha=(self._nodes[iPlusOne+k]-self._nodes[iPlusOne-3+k+i])
+                alpha=(self._nodes[iPlusOne+k]-self._nodes[iPlusOne-self._polDeg+k+i])
                 if alpha!=0:
                     alpha=(self._nodes[iPlusOne+k]-u)/alpha
                 d[k]=alpha*d[k]+(1-alpha)*d[k+1]  
         return d[0]
         
     '''
-    returns the second where u is in it (u\in[u_index-1,u_index])
+    returns the second where u is in it (u\in[u_{index-1},u_index))
     '''
     def getHotInterval(self,u):
         # find hot interval
-        return (self._nodes > u). argmax ()
-    
+        # special case if u==u_K
+        if u==self._nodes[-1]:
+            return (self._nodes == u). argmax ()
+        else:
+            return (self._nodes > u). argmax ()
     #Plot function. The function plots the spline between defined nodes. It also plots the 
     #control points.
     def plot(self):
         us=linspace(self._nodes[2],self._nodes[-3])
         ss=zeros((2,len(us))) #Value of the splines for the inputs us
+        print(us)        
         for k in range(len(us)):
             ss[:,k]=s(us[k])
         contPoints=self.getControlPoints()
@@ -81,7 +88,7 @@ class CSpline:
             factors=array([0.,0.])
             # find hot interval
             indexHotInt=self.getHotInterval(u)
-            indexDiff=indexHotInt-j-1
+            indexDiff=indexHotInt-j
             if indexDiff>=0 and indexDiff<=self._polDeg:
                 basisArr[indexDiff]=1
             else:
@@ -111,11 +118,49 @@ class CSpline:
                     basisArr[i-j]=factors[0]*basisArr[i-j]+factors[1]*basisArr[i-j+1]
             return basisArr[0]
         return basisFunc
+    #generates the control points which generates the interpolated curve of the x values
+    @classmethod
+    def makeDeBoorPoints(cls,nodes,x,y):
+        lenNodes=len(nodes)
+        #size of the vector and matrix
+        lenSystem=lenNodes-2
+        if lenNodes<3:
+            raise Exception("You have to give at least 3 nodes")
+        if nodes[0]!=nodes[1] or nodes[1]!=nodes[2]:
+            raise Exception("The first 3 nodes needs to have the same value")
+        if nodes[-1]!=nodes[-2] or nodes[-2]!=nodes[-3]: 
+            raise Exception("The last 3 nodes needs to have the same value")           
+        #calculate the xi's
+        xis=array([(nodes[i]+nodes[i+1]+nodes[i+2])/3. for i in range(0,lenSystem)])
+        print(xis)        
+        c=CSpline(array(lenSystem*[0]),nodes)        
+        #calculate the matrix with the basisfunctions
+        #for i in range(0,lenSystem):
+        #    for j in range(0,lenSystem):
+        #        a=c.getBasisFunction(i)(xis[j])
+        matA=array([array([c.getBasisFunction(i)(xis[j]) for i in range(0,lenSystem)]) for j in range(0,lenSystem)])        
+        print(matA)
+        return matA
+        #todo: matrix last line (0-line???)
+        #todo: solve the system
+        matB=[x,y]
+        #matA*d=matB => solve the system
+        return solve(matA,matB)
         
-nodes=array([0,1,2,3,4,7,8])
-s=CSpline(array([[1,2],[3,4],[3,5],[3,6],[4,6]]),nodes)
-u=3
-print(s(u))
-for i in range(0,5):
-    print(s.getBasisFunction(1)(nodes[i]))
+x=array([1,2,3,4,5])
+y=array([1,2,3,4,5])
+nodes=array([0,0,1,3,4,4,4])
+s=CSpline(array([[1,2],[3,4],[1,5],[3,6],[4,6]]),nodes)
+s.plot()
+#s.getBasisFunction(3)(4)
+#u=3.66
+#print(s(u))
+#for i in range(0,5):
+#    print(s.getBasisFunction(i)(0))
+#    print(s.getBasisFunction(i)(1))
+#    print(s.getBasisFunction(i)(2.34))
+#    print(s.getBasisFunction(i)(3.67))
+#    print(s.getBasisFunction(i)(4))
+#    print()
 #print(s._controlPoints)
+#CSpline.makeDeBoorPoints(nodes,x,y)
