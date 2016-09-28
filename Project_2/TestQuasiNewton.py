73a4da9fdf2247c54cd11c5ad3e35d2c67b6552a
AnMiKa <alphasierra@gmx.de>
2016-09-29 00:07:35 +0200

    typos

--- Project_2/TestQuasiNewton.py
+++ Project_2/TestQuasiNewton.py
@@ -95,10 +95,10 @@ class TestQuasiNewton(unittest.TestCase):
         solver = QuasiNewton(prob,MethodType.CLASSICALNEWTON)
         tol=1e-5
         kmax=50
-        x0 = transpose(np.array([0.0,0.0]))
+        x0 = np.transpose(np.array([0.0,0.0]))
         (solution,fval,k) = solver.solve(x0,tol,kmax)
         self.assertAlmostEqual(fval,0)
-        np.testing.assert_allclose(solution,array([1,1]),0,1e-5)
+        np.testing.assert_allclose(solution,np.array([1,1]),0,1e-5)
         #print(solution)    
 
     def testRosenBrockLineSearch(self):
@@ -110,10 +110,10 @@ class TestQuasiNewton(unittest.TestCase):
         solver = QuasiNewton(prob,MethodType.ClassicalNewtonExactLineSteepest)
         tol=1e-5
         kmax=50
-        x0 = transpose(np.array([0.0,0.0]))
+        x0 = np.transpose(np.array([0.0,0.0]))
         (solution,fval,k) = solver.solve(x0,tol,kmax)
         self.assertAlmostEqual(fval,0)
-        np.testing.assert_allclose(solution,array([1,1]),0,1e-5)
+        np.testing.assert_allclose(solution,np.array([1,1]),0,1e-5)
         #print(solution)
         
     def testRosenBrockNewtonVsLineSearch(self):
@@ -125,7 +125,7 @@ class TestQuasiNewton(unittest.TestCase):
         solver2 = QuasiNewton(prob,MethodType.ClassicalNewtonExactLineSteepest)
         tol=1e-5
         kmax=50
-        x0 = transpose(np.array([0.0,0.0]))
+        x0 = np.transpose(np.array([0.0,0.0]))
         (solution1,fval1,k1) = solver1.solve(x0,tol,kmax)
         (solution2,fval2,k2) = solver2.solve(x0,tol,kmax)
         self.assertAlmostEqual(fval1,fval2)
@@ -141,11 +141,17 @@ class TestQuasiNewton(unittest.TestCase):
         solver = QuasiNewton(prob,MethodType.ClassicalNewtonInexactLine)
         tol=1e-5
         kmax=50
-        x0 = transpose(np.array([0.0,0.0]))
+        x0 = np.transpose(np.array([0.0,0.0]))
         (solution,fval,k) = solver.solve(x0,tol,kmax)
         self.assertAlmostEqual(fval,0)
-        np.testing.assert_allclose(solution,array([1,1]),0,1e-5)
+        np.testing.assert_allclose(solution,np.array([1,1]),0,1e-5)
         #print(solution)
+
+    def testInexLineOnRosenbrock(self):
+        """
+        Tests the Inexact linesearch as a normal optimizer on the Rosenbrock function
+        """
+        
         
     def testChebyquad(self):
         #testing the chebyshev polynomial
