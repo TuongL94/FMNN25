d07da442ea8502ac4ff0e8f12fdce69384aa982d
AndersH92 <ife_anders@hotmail.com>
2016-10-05 19:10:40 +0200

    "Simple" Mesh
    Mesh that is initialized with a node matrix, room number and a stepsize.

--- Project_3/Mesh.py
+++ Project_3/Mesh.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct  1 09:39:00 2016
-
 @author: Anders Hansson, Tuong Lam, Bernhard Pöchtrager, Annika Stegie
 """
 import scipy as sp
@@ -10,91 +9,50 @@ import pylab as pl
 
 class Mesh():
     """
-    class representing the mesh which discretizes the rooms in project 3
+    Class representing the mesh of nodes that constitutes a room
     """
     
-    def __init__(self,xLength,yLength,meshsize):
+    def __init__(self,nodeMatrix,roomNbr,stepSize):
         """
-        sets up an instance of the mesh-class
+        Sets up an instance of the mesh-class
         input parameters: 
-            dimensions of the room (length in x- and y-direction): xLength, yLength
-            distance between the nodes: meshsize
+            :nodeMatrix: A matrix (array) of Nodes
+            :roomNbr: The number of the room
+            :stepSize: The distance between two nodes in the node matrix
         """
-        self._xLength = xLength
-        self._yLength = yLength
-        self._meshsize = meshsize
+        self._nodeMatrix=nodeMatrix
+        self._roomNbr=roomNbr
+        self._stepSize=stepSize
         
     
-    def getXLength(self):
-        """
-        get-function for the length of the mesh in x-direction
-        return: x-length of the mesh
-        """
-        return self._xLength
+    def getNodeMatrix(self):
+        '''
+        Get function for the nodeMatrix
+        '''
+        return self._nodeMatrix
+    
+    def setNodeMatrix(self,nodeMatrix):
+        '''
+        Set function for the nodeMatrix
+        '''
+        self._nodeMatrix=nodeMatrix
         
-    def getYLength(self):
-        """
-        get-function for the length of the mesh in y-direction
-        return: y-length of the mesh
-        """
-        return self._yLength
+    def getRoomNbr(self):
+        '''
+        Get function for the room number
+        '''
+        return self._roomNbr
         
-    def getMeshsize(self):
-        """
-        get-function for the meshsize of the mesh 
-        return: meshsize
-        """
-        return self._meshsize
+    def setRoomNbr(self,rNbr):
+        '''
+        Set function for the room number (not allowed)
+        '''
+        raise Exception('You are not allowed to change the room number!')
+    
         
-    def setXLength(self,xLength):
-        """
-        set-function for the x-length (not allowed to use)
-        """
-        raise Exception('You are not allowed to change the dimensions of the mesh!')
-        
-    def setYLength(self,yLength):
-        """
-        set-function for the y-length (not allowed to use)
-        """
-        raise Exception('You are not allowed to change the dimensions of the mesh!')
+    nodeMatrix=property(getNodeMatrix,setNodeMatrix)      
+    roomNbr=property(getRoomNbr,setRoomNbr)
+          
+    
+  
 
-    def setMeshsize(self,meshsize):
-        """
-        set-function for the meshsize (not allowed to use)
-        """
-        raise Exception('You are not allowed to change the meshsize!')
-    
-    xLength=property(getXLength,setXLength)
-    yLength=property(getYLength,setYLength)
-    meshsize=property(getMeshsize,setMeshsize)       
-        
-    def createNodes(self):
-        # create instances of the node-class for the given mesh?
-        # implement actual algorithm
-        """
-        return:
-        """
-        return #list of all the node-objects reated?    
-    
-    #def solveMesh(self,...):
-        # check which input arguments are missing
-        # implement the actual function
-        """
-        function solving the discretized Laplace-equation for the given mesh
-        return: data that can be used for a plot
-        """
-        # repeatedly calls solveNode to solve the linear system arrising from 
-        # discretized Laplace equation at every node?
-        # ordering the data in a sensible output type
-        #return
-        
-    #def solveNode(self,...) :
-        # check which input arguments are missing
-        # implement the actual function
-        """
-        function solving the discretized Laplace-equation at a single node
-        assigns the calculated function value of the node to the the property 
-        _funVal of the node
-        return: something the solveMesh function can work with
-        """
-        #return   
