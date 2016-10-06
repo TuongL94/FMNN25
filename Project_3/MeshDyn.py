08bd61200bf2ca52d964535b7ebc50c06b0ccfe4
AnMiKa <alphasierra@gmx.de>
2016-10-06 09:37:58 +0200

    start of dynamic mesh class

---
+++ Project_3/MeshDyn.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  1 09:39:00 2016
+
+@author: Anders Hansson, Tuong Lam, Bernhard Pöchtrager, Annika Stegie
+"""
+import scipy as sp
+import numpy as np
+import pylab as pl
+import Node
+
+# imports for the plotting function
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+
+
+class MeshDyn():
+    """
+    class representing the mesh which discretizes the rooms in project 3
+    """
+    
+    def __init__(self,xLength,yLength,meshsize):
+        """
+        sets up an instance of the mesh-class
+        input parameters: 
+            dimensions of the room (length in x- and y-direction): xLength, yLength
+            distance between the nodes: meshsize
+        """
+        self._xLength = xLength
+        self._yLength = yLength
+        self._meshsize = meshsize
+        
+    
+    def getXLength(self):
+        """
+        get-function for the length of the mesh in x-direction
+        return: x-length of the mesh
+        """
+        return self._xLength
+        
+    def getYLength(self):
+        """
+        get-function for the length of the mesh in y-direction
+        return: y-length of the mesh
+        """
+        return self._yLength
+        
+    def getMeshsize(self):
+        """
+        get-function for the meshsize of the mesh 
+        return: meshsize
+        """
+        return self._meshsize
+        
+    def setXLength(self,xLength):
+        """
+        set-function for the x-length (not allowed to use)
+        """
+        raise Exception('You are not allowed to change the dimensions of the mesh!')
+        
+    def setYLength(self,yLength):
+        """
+        set-function for the y-length (not allowed to use)
+        """
+        raise Exception('You are not allowed to change the dimensions of the mesh!')
+
+    def setMeshsize(self,meshsize):
+        """
+        set-function for the meshsize (not allowed to use)
+        """
+        raise Exception('You are not allowed to change the meshsize!')
+    
+    xLength=property(getXLength,setXLength)
+    yLength=property(getYLength,setYLength)
+    meshsize=property(getMeshsize,setMeshsize)    
+    
+        def plotRoomPart(self):
+            """
+            surface plot of the temperature in one roompart
+            return: a figure object tha shows the plot
+            """
+            fig = plt.figure()
+            ax = fig.gca(projection='3d')
+            X = np.arange(-self.meshsize, self.xLength+.self.meshsize, \
+            self.meshsize)
+            Y = np.arange(-0.self.meshsize, self.yLength+.self.meshsize, self.meshsize)
+            X, Y = np.meshgrid(X, Y)
+            numberOfXNodes = self.xLength/self.meshsize+1
+            numberOfYNodes = self.yLength/self.meshsize+1
+            Z = np.array([[self.nodeMatrix[i,j].funcVal for i in range(numberOfYNodes)] \
+                for j in range(numberOfXNodes)])
+            surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+            # set limits for z-axis
+            ax.set_zlim(np.amin(Z)-self.meshsize, np.amax(Z)+self.meshsize)
+            # don't know what these two lines are for
+            # x.zaxis.set_major_locator(LinearLocator(10))
+            # ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+            # don't know what these two lines are for
+            fig.colorbar(surf, shrink=0.5, aspect=5)
+            plt.show()    
+            return fig
+    
+    # should be part of the init-function
+#    def createNodes(self):
+#        # create instances of the node-class for the given mesh?
+#        # implement actual algorithm
+#        """
+#        return:
+#        """
+#        # What do we do if stepsize doesn't devide wdth or length exactly?
+#        # calculate the number off needed nodes in x- and y-direction        
+#        numberOfXNodes = self._xLength/self._meshsize+1
+#        numberOfYNodes = self._yLength/self._meshsize+1
+#        # preallocate a matrix to store the nodes in it
+#        mesh = np.empty(numberOfYNodes,numberOfXNodes,dtype=object)
+#        for i in range(numberOfYNodes):
+#            for j in range(numberOfXNodes):
+#                mesh[:,:] = Node(self._meshsize*i,self._meshsize*j,'inner')
+#        for j in range(numberOfXNodes):
+#            mesh[,]
+#        return mesh
+    
+    #def solveMesh(self,...):
+        # check which input arguments are missing
+        # implement the actual function
+        """
+        function solving the discretized Laplace-equation for the given mesh
+        return: data that can be used for a plot
+        """
+        # repeatedly calls solveNode to solve the linear system arrising from 
+        # discretized Laplace equation at every node?
+        # ordering the data in a sensible output type
+        #return
+        
+    #def solveNode(self,...) :
+        # check which input arguments are missing
+        # implement the actual function
+        """
+        function solving the discretized Laplace-equation at a single node
+        assigns the calculated function value of the node to the the property 
+        _funVal of the node
+        return: something the solveMesh function can work with
+        """
+        #return   
