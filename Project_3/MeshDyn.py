69be1e74c6f901bb97ae869ff1ffd469706a5cef
AnMiKa <alphasierra@gmx.de>
2016-10-06 12:45:57 +0200

    dynamical solve matrix

--- Project_3/MeshDyn.py
+++ Project_3/MeshDyn.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 class MeshDyn():
     """
     class representing the mesh which discretizes the rooms in project 3
+    teaser how the class could be designed for arbitrary room contallations    
     """
     
     def __init__(self,xLength,yLength,meshsize):
@@ -31,6 +32,10 @@ class MeshDyn():
         self._xLength = xLength
         self._yLength = yLength
         self._meshsize = meshsize
+        self.numberOfXNodes = self.xLength/self.meshsize+1
+        self.numberOfYNodes = self.yLength/self.meshsize+1
+        # some more input parameter
+        # algo to set up the nodeMatrix
         
     
     def getXLength(self):
@@ -54,6 +59,8 @@ class MeshDyn():
         """
         return self._meshsize
         
+        # more get-functions for the input parameter
+        
     def setXLength(self,xLength):
         """
         set-function for the x-length (not allowed to use)
@@ -72,9 +79,12 @@ class MeshDyn():
         """
         raise Exception('You are not allowed to change the meshsize!')
     
+    # more set functions for the other input parameter    
+    
     xLength=property(getXLength,setXLength)
     yLength=property(getYLength,setYLength)
-    meshsize=property(getMeshsize,setMeshsize)    
+    meshsize=property(getMeshsize,setMeshsize) 
+    # more properties
     
         def plotRoomPart(self):
             """
@@ -87,10 +97,8 @@ class MeshDyn():
             self.meshsize)
             Y = np.arange(-0.self.meshsize, self.yLength+.self.meshsize, self.meshsize)
             X, Y = np.meshgrid(X, Y)
-            numberOfXNodes = self.xLength/self.meshsize+1
-            numberOfYNodes = self.yLength/self.meshsize+1
-            Z = np.array([[self.nodeMatrix[i,j].funcVal for i in range(numberOfYNodes)] \
-                for j in range(numberOfXNodes)])
+            Z = np.array([[self.nodeMatrix[i,j].funcVal for i in range(self.numberOfYNodes)] \
+                for j in range(self.numberOfXNodes)])
             surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
                                linewidth=0, antialiased=False)
             # set limits for z-axis
@@ -122,6 +130,127 @@ class MeshDyn():
 #        for j in range(numberOfXNodes):
 #            mesh[,]
 #        return mesh
+
+    def setupSolveMatrixAndRhs(self):
+        """
+        sets up the matrix and right hand side needed to solve the linear system
+        arising from the finite difference discretization of the Laplacian
+        return: matrix and right hand side
+        """
+        # could use the fact, that lapA will be a banded matrix
+        # feature for future work
+        numberOfNodes = self.numberOfXNodes*self.numberOfYNodes
+        #numberOfDirNodes  = 0
+        #for i in range(self.numberOfYNodes):
+        #    for j in range(numberOfXNodes):
+        #        if self.nodeMatrix.nodeType == 'Dirichlet':
+        #            numberOfDirNodes = numberOfDirNodes+1                   
+        #lapA = np.zeros(numberOfNodes-numberOfDirNodes,numberOfNodes-numberOfDirNodes)
+        lapA = np.zeros(numberOfNodes,numberOfNodes)
+        #rhs = np.zeros(numberOfNodes-numberOfDirNodes)
+        rhs = np.zeros(numberOfNodes)        
+        counter = 0
+        for i in range(self.numberOfYNodes):
+            for j in range(self.numberOfXNodes):
+                # what to do when node is a Dirichlet node
+                if self.nodeMatrix[i,j].nodeType == 'Dirichlet':
+                    # write 1 on the corresponding diagonal position of the solvematrix
+                    lapA[counter,counter] = 1
+                    #write the prescribed boundary value on the corresponding position
+                    # on the right hand side
+                    rhs[counter] = self.nodeMatrix[i,j].funcVal
+                    counter = counter+1
+                # what to do when node is an inner node    
+                elif self.nodeMatrix[i,j].nodeType == 'inner':
+                    # write -4 on the diagonal
+                    lapA[counter,counter] = -4
+                    # check the 4 adjacant nodes
+                    # [i-1,j]
+                    if self.nodeMatrix[i-1,j].nodeType == 'inner':
+                        lapA[counter,counter-1] = 1     
+                    elif self.nodeMatrix[i-1,j].nodeType == 'Dirichlet':
+                        rhs[counter] = rhs[counter]-self.nodeMatrix[i-1,j].funcVal                                
+                    else:
+                        lapA[counter,counter-1] = 1
+                    # [i+1,j]
+                    if self.nodeMatrix[i+1,j].nodeType == 'inner':
+                        lapA[counter,counter+1] = 1     
+                    elif self.nodeMatrix[i+1,j].nodeType == 'Dirichlet':
+                        rhs[counter] = rhs[counterY]-self.nodeMatrix[i+1,j].funcVal                                
+                    else:
+                        lapA[counter,counter+1] = 1
+                    # [i,j-1]
+                    if self.nodeMatrix[i,j-1].nodeType == 'inner':
+                        lapA[counter,counter-self.numberOfXNodes] = 1     
+                    elif self.nodeMatrix[i,j-1].nodeType == 'Dirichlet':
+                        rhs[counter] = rhs[counter]-self.nodeMatrix[i,j-1].funcVal                                 
+                    else:
+                        lapA[counter,counter-self.numberOfXNodes] = 1
+                    # [i,j+1]
+                    if self.nodeMatrix[i,j+1].nodeType == 'inner':
+                        lapA[counter,counter+self.numberOfXNodes] = 1     
+                    elif self.nodeMatrix[i,j+1].nodeType == 'Dirichlet':
+                         rhs[counter] = rhs[counter]-self.nodeMatrix[i,j+1].funcVal                               
+                    else:
+                        lapA[counter,counter+self.numberOfXNodes] = 1
+                    counter = counter+1
+                # what to do when node is a Neumann node
+                else:
+                    #write -3 on the diagonal
+                    lapA[counter,counter] = -3 
+                    # check the three adjacant nodes
+                    # find the 'nonexisting' one by catching an index exception?
+                    # [i-1,j]
+                    try:
+                        self.nodeMatrix[i-1,j]
+                        if self.nodeMatrix[i-1,j].nodeType == 'inner':
+                            lapA[counter,counter-1] = 1                        
+                        elif self.nodeMatrix[i-1,j].nodeType == 'Dirichlet':
+                            rhs[counter] = rhs[counter]-self.nodeMatrix[i-1,j].funcVal                                
+                        else:
+                            lapA[counter,counter-1] = 1
+                    # set derivative on right hand side
+                    except IndexError:
+                        rhs[counter] = self.nodeMatrix[i-1,j].deriv
+                    # [i+1,j]
+                    try:
+                        self.nodeMatrix[i+1,j]        
+                        if self.nodeMatrix[i+1,j].nodeType == 'inner':
+                            lapA[counter,counter+1] = 1     
+                        elif self.nodeMatrix[i+1,j].nodeType == 'Dirichlet':
+                            rhs[counter] = rhs[counterY]-self.nodeMatrix[i+1,j].funcVal                                
+                        else:
+                            lapA[counter,counter+1] = 1
+                    # set derivative on right hand side
+                    except IndexError:
+                        rhs[counter] = self.nodeMatrix[i+1,j].deriv
+                    # [i,j-1]
+                    try:
+                        self.nodeMatrix[i,j-1]                    
+                        if self.nodeMatrix[i,j-1].nodeType == 'inner':
+                            lapA[counter,counter-self.numberOfXNodes] = 1     
+                        elif self.nodeMatrix[i,j-1].nodeType == 'Dirichlet':
+                            rhs[counter] = rhs[counter]-self.nodeMatrix[i,j-1].funcVal                                 
+                        else:
+                            lapA[counter,counter-self.numberOfXNodes] = 1
+                    # set derivative on right hand side
+                    except IndexError:
+                        rhs[counter] = self.nodeMatrix[i,j-1].deriv
+                    # [i,j+1]
+                    try:
+                        self.nodeMatrix[i,j+1]
+                        if self.nodeMatrix[i,j+1].nodeType == 'inner':
+                            lapA[counter,counter+self.numberOfXNodes] = 1     
+                        elif self.nodeMatrix[i,j+1].nodeType == 'Dirichlet':
+                             rhs[counter] = rhs[counter]-self.nodeMatrix[i,j+1].funcVal                               
+                        else:
+                            lapA[counter,counter+self.numberOfXNodes] = 1
+                    # set derivative on right hand side
+                    except IndexError:
+                        rhs[counter] = self.nodeMatrix[i,j+1].deriv                           
+                    counter = counter+1   
+        return lapA, rhs
+        
     
     #def solveMesh(self,...):
         # check which input arguments are missing
@@ -134,14 +263,3 @@ class MeshDyn():
         # discretized Laplace equation at every node?
         # ordering the data in a sensible output type
         #return
-        
-    #def solveNode(self,...) :
-        # check which input arguments are missing
-        # implement the actual function
-        """
-        function solving the discretized Laplace-equation at a single node
-        assigns the calculated function value of the node to the the property 
-        _funVal of the node
-        return: something the solveMesh function can work with
-        """
-        #return   
